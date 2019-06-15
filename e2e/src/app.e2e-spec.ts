641bee7bd6951d4ccdf82ef56328200797d4952e
ankur <ankur@kalptaru.in>
2019-06-15 14:30:15 +0530

    testing e2e and unit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,6 +1,7 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
-describe('workspace-project App', () => {
+fdescribe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +10,34 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to james!');
+    expect(page.getParagraphText()).toEqual('james');
+  });
+
+  it('should display Loading component', () => {
+    page.navigateTo();
+    expect(page.checkExistenceOfLoading().count()).toEqual(1);
+  });
+
+  it('should display table first row name', () => {
+    page.navigateTo();
+    expect(page.getFirstRowTableData()).toEqual('rohit');
+  });
+
+  it('should display table row data of "app-user"', () => {
+    page.navigateTo();
+    expect(page.getAllTableData()).toEqual(['ankur', '25', 'bill']);
+  });
+
+  fit('should display updated input value "app-user"', () => {
+    page.navigateTo();
+    page.scrollDownToEnd();
+    browser.sleep(2000);
+    page.clickOnTableRow();
+    browser.sleep(2000);
+    page.clearInput();
+    page.inputTerm('james');
+    browser.sleep(2000);
+    page.clickOnUpdateBtn();
+    expect(page.getTableRowText()).toEqual('james');
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -8,4 +8,44 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  checkExistenceOfLoading() {
+    return element.all(by.css('app-loading'));
+    // return element(by.css('app-root app-loading'));
+  }
+
+  getFirstRowTableData() {
+    return element(by.css('app-root .table tbody tr:nth-child(1) td:nth-child(1)')).getText();
+    // return element(by.css('app-root app-loading'));
+  }
+
+  getAllTableData() {
+    return element.all(by.css('app-user table:nth-child(1) tbody tr:nth-child(1) td')).getText();
+  }
+
+  clickOnTableRow() {
+    return element(by.css('app-user .show-info tbody tr:nth-child(2) td')).click();
+  }
+  getTableRowText() {
+    return element(by.css('app-user .show-info tbody tr:nth-child(2) td')).getText();
+  }
+
+  inputTerm(searchTerm: string) {
+    const input = element.all(by.css('.enter-text'));
+    return input.sendKeys(searchTerm);
+  }
+
+  clearInput() {
+    const input = element.all(by.css('.enter-text'));
+    return input.clear();
+  }
+
+  clickOnUpdateBtn() {
+    return element.all(by.css('.update-table')).click();
+  }
+
+  scrollDownToEnd() {
+    const el = element(by.css('app-user'));
+    return browser.executeScript('arguments[0].scrollIntoView()', el.getWebElement());
+  }
 }
